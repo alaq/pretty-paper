517632f6d7a2f77517681c30bd94e6183dc3dcfb
Adrien Lacquemant <verblodung@gmail.com>
2017-10-26 17:36:19 -0400

    initial styling inspired by Bulma

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
 import React, { Component } from 'react';
-import ArticleCard from './ArticleCard';
+import { SortableContainer, SortableElement, arrayMove } from 'react-sortable-hoc'; // sortable
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import FaNewspaper from 'react-icons/lib/fa/newspaper-o';
+import FaPdf from 'react-icons/lib/fa/file-pdf-o';
+import FaClose from 'react-icons/lib/fa/times-circle';
+
+
+const SortableItem = SortableElement((props) => {
+  return (
+  <li className="card">
+    <div className="thumb-div">
+      <img src={props.article.lead_image_url} />
+    </div>
+    <div className="text-div">
+      <strong>{props.article.title}</strong>
+      <small>{props.article.domain} · by {props.article.author}</small>
+      <p>{props.article.excerpt}</p>
+      <small>Today · 7 minutes</small>
+    </div>
+    <div className="close-div">
+      <FaClose className="close-icon" onClick={() => props.deleteFunc(props.article.url)} />
+    </div>
+
+  </li>
+  )
+}
+);
+
+const SortableList = SortableContainer((props) => {
+  const { deleteFunc } = props;
+  const { items } = props;
+  return (
+    <ul className="article-list">
+      {items.map((article, index) => (
+        <SortableItem key={`article-${index}`} index={index} article={article} deleteFunc={deleteFunc}/>
+      ))}
+    </ul>
+  );
+});
 
 class App extends Component {
 
-  constructor () {
+  constructor() {
     super();
     this.state = {
       urls: '',
-      articles: []
+      articles: [],
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -18,11 +55,18 @@ class App extends Component {
     this.export = this.export.bind(this);
   }
 
-  handleChange (evt) {
-    this.setState({url: evt.target.value});
+  // SORTABLE
+  onSortEnd = ({oldIndex, newIndex}) => {
+    this.setState({
+      articles: arrayMove(this.state.articles, oldIndex, newIndex),
+    });
+  };
+
+  handleChange(evt) {
+    this.setState({ urls: evt.target.value });
   }
 
-  addToArticleList (url) {
+  addToArticleList(url) {
     url = 'https://mercury.postlight.com/parser?url=' + url;
     axios({
       url: url,
@@ -31,57 +75,61 @@ class App extends Component {
         'Content-Type': 'application/json'
       }
     }).then(response => {
-      this.setState({articles: this.state.articles.concat(response.data)})})
-    .catch(console.error);
+      this.setState({ articles: this.state.articles.concat(response.data) })
+    })
+      .catch(console.error);
   }
 
-  handleSubmit (evt) {
+  handleSubmit(evt) {
     evt.preventDefault();
     this.setState({
       urls: evt.target.urls.value
     })
-    const urls = evt.target.urls.value;
+    let urls = evt.target.urls.value;
 
     var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
     var regex = new RegExp(expression);
-    urls.match(regex).forEach(url => this.addToArticleList(url));
+    urls = urls.match(regex);
+    urls.forEach(url => this.addToArticleList(url));
 
+    this.setState({urls: ''});
   }
 
-  handleDelete (url) {
+  handleDelete(url) {
+    console.log('delete try', url);
     const articles = this.state.articles.filter(article => article.url !== url);
-    this.setState({articles: articles});
+    this.setState({ articles: articles });
   }
 
-  export () {
+  export() {
     this.state.articles.forEach(article => console.dir(article));
   }
 
   render() {
     const { articles } = this.state;
+    console.log(articles);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Pretty Paper</h1>
-          <p className="App-intro">
-            Print your reading list.
-          </p>
+          Print your reading list.
+          <form onSubmit={this.handleSubmit}>
+            <textarea
+              value={this.state.urls}
+              onChange={this.handleChange}
+              type="text"
+              name="urls"
+              placeholder="Add your articles here, separated by spaces, commas, or semicolons... or even URLs in text."
+            />
+            <div>
+              {/* <input className="mainButton" type="submit" value="Fetch!" /> */}
+              <button className="mainButton blue" type="submit"><FaNewspaper /> Add the <strong>Articles</strong></button>
+              <button className="mainButton green" onClick={this.export}><FaPdf /> Generate <strong>PDF</strong></button>
+            </div>
+          </form>
         </header>
-        <form onSubmit={this.handleSubmit}>
-          <textarea
-            value={this.state.url}
-            onChange={this.handleChange}
-            type="text"
-            name="urls"
-            placeholder="Add your articles here, separated by spaces, commas, or semicolons..."
-             />
-          <input type="submit" value="Fetch!" />
-        </form>
-        <ul>
-          { articles && articles.map(article => <ArticleCard key={article.url} article={article} delete={this.handleDelete} /> )}
-        </ul>
-        <button onClick={this.export}>PDF</button>
+        <SortableList items={this.state.articles} onSortEnd={this.onSortEnd} deleteFunc={this.handleDelete} />
       </div>
     );
   }
